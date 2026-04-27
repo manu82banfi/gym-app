5a3d605c6f7aa4ad5d6d9792c2bad69194b65fde
manu82banfi <manu82.banfi@gmail.com>
2026-04-27 20:42:36 +0200

    Add files via upload

--- cloud.js
+++ cloud.js
@@ -8,6 +8,7 @@ async function caricaCloud() {
     const r = await fetch(BASE_URL, {
       headers: { "X-Master-Key": API_KEY }
     });
+
     const j = await r.json();
     return j.record || [];
   } catch {
